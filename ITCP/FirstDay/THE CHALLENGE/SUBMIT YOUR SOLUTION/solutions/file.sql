f68245196c074a08db5e96da3aaa25b1ec840d87
Abderrezake <118079812+Abderrezake@users.noreply.github.com>
2022-12-25 14:10:33 +0100

    Update file.sql

--- ITCP/FirstDay/THE CHALLENGE/SUBMIT YOUR SOLUTION/solutions/file.sql
+++ ITCP/FirstDay/THE CHALLENGE/SUBMIT YOUR SOLUTION/solutions/file.sql
@@ -1,1 +1,11 @@
         CREATE DATABASE itCDB;
+        
+CREATE TABLE table_name (
+    column1 char,
+    column2 integer,
+    column3 integer,
+   
+); 
+UPDATE table_name
+SET column3 = 50, column2 = 30, 
+WHERE condition; 
